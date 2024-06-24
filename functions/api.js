810f9ab8b47fa0fa63c270ac93c330ec3a740fc5
silviaphungky <silvia.phungky@mandirisek.co.id>
2024-06-24 23:20:58 +0700

    add put method

--- functions/api.js
+++ functions/api.js
@@ -2,8 +2,8 @@ const express = require('express')
 require('dotenv').config()
 
 const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json({ limit: '1mb' }))
+app.use(express.urlencoded({ extended: false, limit: '1mb' }))
 const serverless = require('serverless-http')
 
 // import
@@ -28,15 +28,13 @@ router.get('/', (req, res) => {
 
 router.post('/animation', (req, res) => {
   try {
-    const { json } = req.body
     Animation.create(req.body).then((animation) =>
       res
         .status(201)
         .json({ message: 'Successfully create animation', data: animation })
     )
   } catch (error) {
-    res.status(500)
-    console.log(error, 'Failed create animation')
+    res.status(500).json({ error: 'Internal server error' })
   }
 })
 
@@ -46,7 +44,22 @@ router.get('/animation/:id', async (req, res) => {
     const animation = await Animation.findById(id)
     res.status(200).json({ animation })
   } catch (error) {
-    res.status(500)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+})
+
+router.put('/animation/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    Animation.findByIdAndUpdate({ _id: id }, req.body, { new: true }).then(
+      (animation) =>
+        res.status(200).json({
+          message: `Successfully update animation ${id}`,
+          animation,
+        })
+    )
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' })
   }
 })
 
